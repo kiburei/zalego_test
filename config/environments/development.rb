cf6667cefab4d456c1ae279852f2383839a95be3
kiburei <akiburei@gmail.com>
2018-04-16 17:58:46 +0300

    good

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,21 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host:'localhost', port: '3000' }
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "localhost:3000",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "akiburei@gmail.com",
+    password: "11231991"
 
-  config.action_mailer.perform_caching = false
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
